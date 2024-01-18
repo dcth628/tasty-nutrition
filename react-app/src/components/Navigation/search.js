00f384b032a1489e4541517961ec1f150ef3620b
Dean <dcth628@gmail.com>
2024-01-17 21:48:30 -0800

    updated recipe all

--- react-app/src/components/Navigation/search.js
+++ react-app/src/components/Navigation/search.js
@@ -9,7 +9,7 @@ import './Search.css'
 const SearchBar = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const [input , setInput ] = useState("");
+    const [input, setInput] = useState("");
 
     const results = useSelector(state => state.search);
     const ingredients = results.ingredients;
@@ -53,51 +53,51 @@ const SearchBar = () => {
     return (
         <div className="search-bar">
             <input
-            type="search"
-            className="search-input"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onFocus={() => show()}
-            onBlur={(e)=> hide(e)}
-            placeholder="Search..."
-             />
+                type="search"
+                className="search-input"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onFocus={() => show()}
+                onBlur={(e) => hide(e)}
+                placeholder="Search..."
+            />
             <div className="search-container" onBlur={(e) => hide(e)}>
                 <div className="search-results hidden" >
                     {recipes && (recipes.length > 0 && input?.length > 0 ?
-                    <div>
-                        <div className="search-card-title" onMouseDown={(e) => hide(e)}>Recipes</div>
-                        {recipes.map((recipe) => (
-                            <div key={recipe.id} className="search-card" onMouseDown={() => reset1(recipe.id)}>
-                                {recipe.name}
-                            </div>
-                        ))}
-                    </div>
-                    : (input?.length > 0 ? <div className="search-none">No Recipe Match</div> :
-                    <div className="search-none hidden"> No Recipe Match</div>)
-                        )}
-                    {cookbooks && (cookbooks.length > 0 && input?.length > 0 ?
-                    <div>
-                        <div className="search-card-title" onMouseDown={(e) => hide(e)}>Cookbooks</div>
-                    {cookbooks.map((cookbook) => (
-                        <div key={cookbook.id} className="search-card" onMouseDown={() => reset2(cookbook.id)}>
-                            {cookbook.name}
+                        <div>
+                            <div className="search-card-title" onMouseDown={(e) => hide(e)}>Recipes</div>
+                            {recipes.map((recipe) => (
+                                <div key={recipe.id} className="search-card" onMouseDown={() => reset1(recipe.id)}>
+                                    {recipe.name}
+                                </div>
+                            ))}
                         </div>
-                    )) }
-                    </div>
-                    : (input?.length > 0 ? <div className="search-none">No Cookbook Match</div> :
-                    <div className="search-none hidden">No Cookbook Match</div>)
+                        : (input?.length > 0 ? <div className="search-none">No Recipe Match</div> :
+                            <div className="search-none hidden"> No Recipe Match</div>)
+                    )}
+                    {cookbooks && (cookbooks.length > 0 && input?.length > 0 ?
+                        <div>
+                            <div className="search-card-title" onMouseDown={(e) => hide(e)}>Cookbooks</div>
+                            {cookbooks.map((cookbook) => (
+                                <div key={cookbook.id} className="search-card" onMouseDown={() => reset2(cookbook.id)}>
+                                    {cookbook.name}
+                                </div>
+                            ))}
+                        </div>
+                        : (input?.length > 0 ? <div className="search-none">No Cookbook Match</div> :
+                            <div className="search-none hidden">No Cookbook Match</div>)
                     )}
                     {ingredients && (ingredients.length > 0 && input?.length > 0 ?
-                    <div>
-                        <div className="search-card-title" onMouseDown={(e) => hide(e)}>Ingredients</div>
-                    {ingredients.map((ingredient) => (
-                        <div key={ingredient.id} className="search-card" onMouseDown={() => reset3(ingredient.id)}>
-                            {ingredient.name}
+                        <div>
+                            <div className="search-card-title" onMouseDown={(e) => hide(e)}>Ingredients</div>
+                            {ingredients.map((ingredient) => (
+                                <div key={ingredient.id} className="search-card" onMouseDown={() => reset3(ingredient.id)}>
+                                    {ingredient.name}
+                                </div>
+                            ))}
                         </div>
-                    )) }
-                    </div>
-                    : (input?.length > 0 ? <div className="search-none">No Ingredient Match</div> :
-                    <div className="search-none hidden">No Ingredient Match</div>)
+                        : (input?.length > 0 ? <div className="search-none">No Ingredient Match</div> :
+                            <div className="search-none hidden">No Ingredient Match</div>)
                     )}
                 </div>
             </div>
